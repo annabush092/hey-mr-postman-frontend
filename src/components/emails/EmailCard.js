66b36bf7ab1f0092aa91bca2ca46bfb208829d64
annabush092 <annabush092@gmail.com>
2017-11-10 14:16:13 -0500

    finished mvp emailcard

--- src/components/emails/EmailCard.js
+++ src/components/emails/EmailCard.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
 
-
 class EmailCard extends Component{
   render(){
     console.log("in EmailCard: ", this.props)
     return(
       <div>
         <ul>
-          <li>To: {this.props.recipient_id}</li>
-          <li>From: {this.props.user}</li>
+          <li>To: {this.props.recipient.email_address}</li>
+          <li>From: {this.props.user.email_address}</li>
           <li>Subject: {this.props.subject}</li>
+          <li>Sent? {`${this.props.sent}`}</li>
+          <li>Read? {`${this.props.read}`}</li>
         </ul>
-        <p>Dear {this.props.recipient_id},</p>
+        <p>Dear {this.props.recipient.name},</p>
         <p>{this.props.content}</p>
         <p>With Love,</p>
-        <p>{this.props.user}</p>
+        <p>{this.props.user.name}</p>
         <hr/>
       </div>
     )
